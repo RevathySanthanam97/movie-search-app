630e8e60d1e072fd48e3add1777c21da45726b05
RevathySanthanam97 <revathysanthanam97@gmail.com>
2021-08-13 13:55:34 +0530

    updated

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 
-export default function MovieCard({movie}){
-
-    return(
-       
-            <div className="card" key={movie.id}>
-              <p className="movieTitle">{movie.title}</p>
-              <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + ' poster'}/>
-              <p className="movieRelease">Release Date: {movie.release_date}</p>
-              <p className="movieRating">Rating: {movie.vote_average}</p>
-              <p className="movieOverview">Summary: {movie.overview}</p>
-            </div>
-    
-    )
+export default function MovieCard({ movie }) {
+  return (
+    <div className="card" key={movie.id}>
+      <p className="movieTitle">{movie.title}</p>
+      <img
+        className="card--image"
+        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+        alt={movie.title + " poster"}
+      />
+      <p className="movieRelease">
+        <strong>Release Date:</strong> {movie.release_date}
+      </p>
+      <p className="movieRating">
+        <strong>Rating:</strong> {movie.vote_average}
+      </p>
+      <p className="movieOverview">
+        <strong>Summary:</strong> {movie.overview}
+      </p>
+    </div>
+  );
 }
